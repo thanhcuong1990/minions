2e2b1808f674232d0f7f6b3237d7cdf90fca2e23
Cuong Lam <thanhcuong1990@gmail.com>
2015-08-02 14:17:14 +0700

    Add univeristies api

--- career_path_ror/app/controllers/api/v1/universities_controller.rb
+++ career_path_ror/app/controllers/api/v1/universities_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::UniversitiesController < ApiController
-  def index
-    type = params[:type]
 
-    response_sucess(questions)
+  def index
+
+    type = params[:type].to_i
+    list_university = []
+    University.all.each do |u|
+      if u.careers.include?(type)
+        list_university << u
+      end
+    end
+    data = {"universities": list_university}
+    response_success_with_data(data)
   end
+
 end
